52e34199a2353bfedc156242cb6a7e323bee99de
Zeroxys <miguelzavalac@gmail.com>
2017-07-12 11:20:38 -0500

    changes in server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express')
-const port = process.env.PORT || 8080
+//const port = process.env.PORT || 8080
 
 const app = new express()
 
-//app.set('port', (process.env.PORT || 80))
+app.set('port', (process.env.PORT || 80))
 app.use(express.static('dist'))
 
 app.get('/', (req,res) => {
   res.status(200).sendfile('./dist/index.html')
 })
 
-app.listen(port, (err) => {
+app.listen(app.get('port'), (err) => {
   if(!err) {
-    return console.log(`Server run on port ${port}`)
+    return console.log(`Server run on port ${app.get('port')}`)
   }
 
   console.log(`Error happened : ${err}`)
