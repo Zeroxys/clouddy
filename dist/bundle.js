5da04a9ce29eab6c3c7b01de073ce26e7b062617
Gabriel Galan Mendez <gabrielgalanmendez@MacBook-Air-de-Gabriel.local>
2017-07-11 19:49:54 -0500

    add style bottom

--- dist/bundle.js
+++ dist/bundle.js
@@ -73,6 +73,13 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__css_style_styl___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__css_style_styl__);
 
 
+/*
+$('#menu-toggle-wrapper').click(function(){
+    $(this).toggleClass('active');
+});
+*/
+
+
 /***/ }),
 /* 1 */
 /***/ (function(module, exports) {
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
 import '../css/style.styl'
+
+/*
+$('#menu-toggle-wrapper').click(function(){
+    $(this).toggleClass('active');
+});
+*/
