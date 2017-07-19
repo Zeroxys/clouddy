2b23a147bc4aecb465e1f2c17eb21d2fe6d8eb62
Zeroxys <miguelzavalac@gmail.com>
2017-07-18 20:47:22 -0500

    conflictos solucionados

--- src/js/index.js
+++ src/js/index.js
@@ -7,73 +7,15 @@ import axios from 'axios'
 
 $(function() {
      
-  Toastr.options = {
-    'hideDuration'  : 1000,
-    'closeDuration' : 1000,
-    'showDuration'  :1000,
-    'onShown'       :1000,
-    'timeOut'       :4000,
-    'positionClass' :'toast-bottom-center',
-  }
-
-  var validator = $("#form").submit(function(e) {
-    e.preventDefault()
-  }).validate({
-
-    submitHandler: function(form) {
-
-      axios.post('/form', {
-        name : 'user',
-        email : 'user@mail.com',
-        text : 'bla bla bla'
-      })
-    },
-
-    invalidHandler: function(event, validator){
-      var errors  = validator.errorList
-
-      if(errors.length <= 2){
-        $.each( errors, function( key, value ) {
-          Toastr.error(value.message);  
-          });
-      }else if(errors.length == 3){
-        Toastr.error('Es necesario que llene el formulario' );  
-      }
-    },
-        
-    showErrors: function(errorMap, errorList) {  
-      return true;   
+    Toastr.options = {
+        'hideDuration'  : 1000,
+        'closeDuration' : 1000,
+        'showDuration'  :1000,
+        'onShown'       :1000,
+        'timeOut'       :4000,
+        'positionClass' :'toast-bottom-center',
     }
-  })
 
-  var alto = $( window ).height();
-  var nvMovil = $('.nav-movil');
-  var contHeader = $('container-header');
-
-
-  //show menu movil
-  $('#menu-toggle-wrapper').click(function(){
-    $(this).toggleClass('active');
-
-    if($(this).hasClass('active')){
-      $('body').css('overflow','hidden')
-      $('#head-logo').css('color','white')
-        nvMovil.css({
-          'height': alto,
-          'display': 'block'
-        })        
-    }else{
-      $('body').css('overflow','auto')
-      $('#head-logo').css('color','#2c6ef0')
-      $('.nav-movil').css('display', 'none')
-    }
-  });
-
-<<<<<<< HEAD
-  //get event click link
-  $('a[href*=\\#]:not([href=\\#])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-=======
    var validator = $("#form").validate({
        errorClass:'invalid',
         rules: {
@@ -162,88 +104,84 @@ $(function() {
             var idSection = target[0].id; 
 
             if (target.length) {
->>>>>>> f77baaa9c37174f7c1ef2519a8ef982647b3b5be
 
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                //movil
+                nvMovil.hide()
+                $('body').css('overflow','auto')
 
-      var idSection = target[0].id; 
+                $('#menu-toggle-wrapper').removeClass('active');
 
-      var that = $(this);
+                //desk
+                if(!$(this).hasClass('active-item')){
+                    $(this).siblings().removeClass('active-item');
+                    $(this).addClass('active-item');
+                }
 
-      if (target.length) {
-        //movil
-        nvMovil.hide()
-        $('body').css('overflow','auto')
+            
+                // condicional para evento scroll con respecto a la funcion
+                idSection == 'services'? scrollToContent(100):scrollToContent(0)
 
-        $('#menu-toggle-wrapper').removeClass('active');
+                function scrollToContent(m){
+                    m==0?$('#head-logo').css('color','#2c6ef0'): $('#head-logo').css('color','white')
 
-        //desk
-        if(!$(this).hasClass('active-item')){
-            $(this).siblings().removeClass('active-item');
-            $(this).addClass('active-item');
+                    $('html,body').animate({
+                        scrollTop: target.offset().top + m
+                    }, 1000);  
+                }
+                    
+                return false;
+            }
         }
-
-        // condicional para evento scroll con respecto a la funcion
-        idSection == 'services'? scrollToContent(100):scrollToContent(0)
-
-        function scrollToContent(m){
-            m==0?$('#head-logo').css('color','#2c6ef0'): $('#head-logo').css('color','white')
-
-            $('html,body').animate({
-                scrollTop: target.offset().top + m
-            }, 1000);  
-        }
-                
-          return false;
-        }
-      }
     });
 
-  var home = $('#home');
-  var services = $('#services');
-  var portfolio = $('#portfolio');
-  var contact = $('#contact');
+    var home = $('#home');
+    var services = $('#services');
+    var portfolio = $('#portfolio');
+    var contact = $('#contact');
 
-  var navA = $('.nav-menu').find('a')
-  var scrollTopWindow
+    var navA = $('.nav-menu').find('a')
+    var scrollTopWindow
 
-  $(window).scroll(function(){
-    scrollTopWindow = $(this).scrollTop();
-      
-      if(scrollTopWindow < services.offset().top){
-        navA.removeClass('active-item');
-        $("[href='#home']").addClass("active-item");  
-      }
+    $(window).scroll(function(){
+        scrollTopWindow = $(this).scrollTop();
+        
+        if(scrollTopWindow < services.offset().top){
+            navA.removeClass('active-item');
+            $("[href='#home']").addClass("active-item");  
+        }
 
-      if(scrollTopWindow > services.offset().top && scrollTopWindow < portfolio.offset().top-100){
-        navA.removeClass('active-item');
-        $("[href='#services']").addClass("active-item");  
-      }
+        if(scrollTopWindow > services.offset().top && scrollTopWindow < portfolio.offset().top-100){
+            navA.removeClass('active-item');
+            $("[href='#services']").addClass("active-item");  
+        }
 
-      if(scrollTopWindow > portfolio.offset().top-10 && scrollTopWindow < contact.offset().top){
-        navA.removeClass('active-item');
-        $("[href='#portfolio']").addClass("active-item");  
-      }
-      if(scrollTopWindow > contact.offset().top-10){
-        navA.removeClass('active-item');
-        $("[href='#contact']").addClass("active-item");  
-      }
+        if(scrollTopWindow > portfolio.offset().top-10 && scrollTopWindow < contact.offset().top){
+            navA.removeClass('active-item');
+            $("[href='#portfolio']").addClass("active-item");  
+        }
+        if(scrollTopWindow > contact.offset().top-10){
+            navA.removeClass('active-item');
+            $("[href='#contact']").addClass("active-item");  
+        }
 
-      if(scrollTopWindow > 40){
 
-        $('.cont-logo .logo').addClass('scroll-logo');
-        $('.container-header').addClass('scroll-container-header');
+        if(scrollTopWindow > 40){
 
-        $('.container-header header').addClass('scroll-header').find('a').addClass('scroll-link');
-      }else{
-        $('#head-logo').css('color','#2c6ef0')
-        $('.cont-logo .logo').removeClass('scroll-logo');
-        $('.container-header').removeClass('scroll-container-header');
+            $('.cont-logo .logo').addClass('scroll-logo');
+            $('.container-header').addClass('scroll-container-header');
 
-        $('.container-header header').removeClass('scroll-header').find('a').removeClass('scroll-link')
-      }
+            $('.container-header header').addClass('scroll-header').find('a').addClass('scroll-link');
+        }else{
+            $('#head-logo').css('color','#2c6ef0')
+            $('.cont-logo .logo').removeClass('scroll-logo');
+            $('.container-header').removeClass('scroll-container-header');
+
+            $('.container-header header').removeClass('scroll-header').find('a').removeClass('scroll-link')
+        }
+
     });
+
+
 });
 
 
