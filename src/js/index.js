ce3db5ecaa93caab8083d94458f0472e9cc1e861
Zeroxys <miguelzavalac@gmail.com>
2017-07-19 00:57:12 -0500

    final bundle

--- src/js/index.js
+++ src/js/index.js
@@ -50,13 +50,13 @@ $(function() {
                 obj.push(val.value)
            });
             //objeto que enviamos 
-          console.log(obj)
+        //   console.log(obj)
 
-          axios.post('http://localhost:8181/api/person', obj).then( (response) => {
-            console.log(response)
-          }).catch((err) => {
-            console.log(`err : ${err}`)
-          })
+        //   axios.post('http://localhost:8181/api/person', obj).then( (response) => {
+        //     console.log(response)
+        //   }).catch((err) => {
+        //     console.log(`err : ${err}`)
+        //   })
     
             Toastr.success('Su mensaje ha sido enviado' );
         },
